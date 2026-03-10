2de623a2b5aa2a4cb734507acabf945c4a402709
AFlo59 <etudessup59230@gmail.com>
2026-03-10 09:15:57 +0100

    INFRA 01 à 03

---
+++ infra/modules/storage/main.tf
@@ -0,0 +1,32 @@
+resource "google_storage_bucket" "raw" {
+  name                        = var.bucket_name
+  location                    = var.location
+  project                     = var.project_id
+  uniform_bucket_level_access = true
+  force_destroy               = var.force_destroy
+
+  versioning {
+    enabled = var.versioning_enabled
+  }
+
+  dynamic "lifecycle_rule" {
+    for_each = var.lifecycle_delete_age_days == null ? [] : [1]
+
+    content {
+      condition {
+        age = var.lifecycle_delete_age_days
+      }
+      action {
+        type = "Delete"
+      }
+    }
+  }
+}
+
+resource "google_storage_bucket_iam_member" "ingestion_object_admin" {
+  count = var.ingestion_sa_email == "" ? 0 : 1
+
+  bucket = google_storage_bucket.raw.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${var.ingestion_sa_email}"
+}
